dc15b01dc693f70bfa956d71dd5d838731ea7319
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2021-02-18 23:15:55 +0100

    Add more properties

--- src/abstractrecord.jl
+++ src/abstractrecord.jl
@@ -1,6 +1,19 @@
 abstract type AbstractRecord end
 
-const recordpropertynames = (:chrom,:filter) # TODO: add more
+const recordpropertynames = (:chrom,
+                             :pos,
+                             :id,
+                             :ref,
+                             :alt,
+                             :qual,
+                             :filter,
+                             :info,
+                             :format,
+                             :genotype,
+                             # :info, # TODO: Add
+                             # :genotype, # TODO: Add
+                            )
+
 
 function Base.propertynames(record::T, private=false) where T<:AbstractRecord
 	private || return recordpropertynames
@@ -9,6 +22,14 @@ end
 
 function Base.getproperty(record::AbstractRecord, sym::Symbol)
 	sym==:chrom && return chrom(record)
+	sym==:pos && return pos(record)
+	sym==:id && return id(record)
+	sym==:ref && return ref(record)
+	sym==:alt && return alt(record)
+	sym==:qual && return qual(record)
 	sym==:filter && return filter(record)
+	sym==:info && return info(record)
+	sym==:format && return format(record)
+	sym==:genotype && return genotype(record)
 	getfield(record,sym)
 end
