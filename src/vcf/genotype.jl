50f39cb876552bd1ec5cbd036b1c6117eef9eebe
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2021-02-21 17:25:31 +0100

    WIP make VCFGenotypeWrapper an AbstractArray

--- src/vcf/genotype.jl
+++ src/vcf/genotype.jl
@@ -1,4 +1,4 @@
-struct VCFGenotypeWrapper
+struct VCFGenotypeWrapper <: AbstractArray{String,2}
     data::Vector{UInt8}
     format::Vector{UnitRange{Int}}
     genotype::Vector{Vector{UnitRange{Int}}}
@@ -12,54 +12,72 @@ end
 genotypewrapper(record::VCFRecord) = VCFGenotypeWrapper(record)
 
 
-# Array-like interface
-function Base.getindex(g::VCFGenotypeWrapper, index::Integer, ::Colon)
-    return genotype_impl(g, index, 1:lastindex(g.format))
-end
+# AbstractArray interface (IndexStyle is IndexCartesian)
+Base.IteratorSize(::VCFGenotypeWrapper) = HasShape{2}()
+Base.size(g::VCFGenotypeWrapper) = (length(g.genotype),length(g.format))
+Base.getindex(g::VCFGenotypeWrapper, i::Int, key::Int) = genotype_impl(g, i, key)
 
-function Base.getindex(g::VCFGenotypeWrapper, index::Integer, key::String)::String
-    k = findgenokey(g, key)
-    if k == nothing
-        throw(KeyError(key))
-    end
-    return genotype_impl(g, index, k)
-end
+# Handling of String keys
+# Base.getindex(g::VCFGenotypeWrapper, i::Int, key::String) = genotype_impl(g, i, findgenokey(g,key))
+Base.getindex(g::VCFGenotypeWrapper, I, key::String) = g[I,findgenokey(g,key)]
+Base.getindex(g::VCFGenotypeWrapper, I, key::AbstractVector{String}) = g[I,findgenokey.(Ref(g),key)]
 
-function Base.getindex(g::VCFGenotypeWrapper, index::Integer, keys::AbstractVector{String})::Vector{String}
-    return [g[index, key] for key in keys]
-end
+# Add other methods for performance?
 
-function Base.getindex(g::VCFGenotypeWrapper, indexes::AbstractVector{T}, key::String)::Vector{String} where T<:Integer
-    k = findgenokey(g, key)
-    if k == nothing
-        throw(KeyError(key))
-    end
-    return [genotype_impl(g, i, k) for i in indexes]
-end
 
-function Base.getindex(g::VCFGenotypeWrapper, indexes::AbstractVector{T}, keys::AbstractVector{String})::Vector{Vector{String}} where T<:Integer
-    ks = Vector{Int}(length(keys))
-    for i in 1:lastindex(keys)
-        key = keys[i]
-        k = findgenokey(g, key)
-        if k == nothing
-            throw(KeyError(key))
-        end
-        ks[i] = k
-    end
-    return [genotype_impl(g, i, ks) for i in indexes]
-end
 
-function Base.getindex(g::VCFGenotypeWrapper, ::Colon, key::String)::Vector{String}
-    return g[1:lastindex(g.genotype), key]
-end
 
+# Old
+# function Base.getindex(g::VCFGenotypeWrapper, index::Integer, ::Colon)
+#     return genotype_impl(g, index, 1:lastindex(g.format))
+# end
+
+# function Base.getindex(g::VCFGenotypeWrapper, index::Integer, key::String)::String
+#     k = findgenokey(g, key)
+#     if k == nothing
+#         throw(KeyError(key))
+#     end
+#     return genotype_impl(g, index, k)
+# end
+
+# function Base.getindex(g::VCFGenotypeWrapper, index::Integer, keys::AbstractVector{String})::Vector{String}
+#     return [g[index, key] for key in keys]
+# end
+
+# function Base.getindex(g::VCFGenotypeWrapper, indexes::AbstractVector{T}, key::String)::Vector{String} where T<:Integer
+#     k = findgenokey(g, key)
+#     if k == nothing
+#         throw(KeyError(key))
+#     end
+#     return [genotype_impl(g, i, k) for i in indexes]
+# end
+
+# function Base.getindex(g::VCFGenotypeWrapper, indexes::AbstractVector{T}, keys::AbstractVector{String})::Vector{Vector{String}} where T<:Integer
+#     ks = Vector{Int}(length(keys))
+#     for i in 1:lastindex(keys)
+#         key = keys[i]
+#         k = findgenokey(g, key)
+#         if k == nothing
+#             throw(KeyError(key))
+#         end
+#         ks[i] = k
+#     end
+#     return [genotype_impl(g, i, ks) for i in indexes]
+# end
+
+# function Base.getindex(g::VCFGenotypeWrapper, ::Colon, key::String)::Vector{String}
+#     return g[1:lastindex(g.genotype), key]
+# end
+
+# End AbstractArray interface
 
 
 
 
 function findgenokey(g::VCFGenotypeWrapper, key::String)
-    return findfirst(r -> isequaldata(key, g.data, r), g.format)
+    j = findfirst(r -> isequaldata(key, g.data, r), g.format)
+    j === nothing && throw(KeyError(key))
+    j
 end
 
 function genotype_impl(g::VCFGenotypeWrapper, index::Int, keys::AbstractVector{Int})
