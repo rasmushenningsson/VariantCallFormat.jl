70d78b4ec4086b146d949b2518c764d83da864b5
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2021-02-22 20:23:24 +0100

    Code cleanup

--- src/vcf/genotype.jl
+++ src/vcf/genotype.jl
@@ -21,56 +21,7 @@ Base.getindex(g::VCFGenotypeWrapper, i::Int, key::Int) = genotype_impl(g, i, key
 Base.getindex(g::VCFGenotypeWrapper, I, key::String) = g[I,findgenokey(g,key)]
 Base.getindex(g::VCFGenotypeWrapper, I, key::AbstractVector{String}) = g[I,findgenokey.(Ref(g),key)]
 
-# Add other methods for performance?
-
-
-
-
-# Old
-# function Base.getindex(g::VCFGenotypeWrapper, index::Integer, ::Colon)
-#     return genotype_impl(g, index, 1:lastindex(g.format))
-# end
-
-# function Base.getindex(g::VCFGenotypeWrapper, index::Integer, key::String)::String
-#     k = findgenokey(g, key)
-#     if k == nothing
-#         throw(KeyError(key))
-#     end
-#     return genotype_impl(g, index, k)
-# end
-
-# function Base.getindex(g::VCFGenotypeWrapper, index::Integer, keys::AbstractVector{String})::Vector{String}
-#     return [g[index, key] for key in keys]
-# end
-
-# function Base.getindex(g::VCFGenotypeWrapper, indexes::AbstractVector{T}, key::String)::Vector{String} where T<:Integer
-#     k = findgenokey(g, key)
-#     if k == nothing
-#         throw(KeyError(key))
-#     end
-#     return [genotype_impl(g, i, k) for i in indexes]
-# end
-
-# function Base.getindex(g::VCFGenotypeWrapper, indexes::AbstractVector{T}, keys::AbstractVector{String})::Vector{Vector{String}} where T<:Integer
-#     ks = Vector{Int}(undef, length(keys))
-#     for i in 1:lastindex(keys)
-#         key = keys[i]
-#         k = findgenokey(g, key)
-#         if k == nothing
-#             throw(KeyError(key))
-#         end
-#         ks[i] = k
-#     end
-#     return [genotype_impl(g, i, ks) for i in indexes]
-# end
-
-# function Base.getindex(g::VCFGenotypeWrapper, ::Colon, key::String)::Vector{String}
-#     return g[1:lastindex(g.genotype), key]
-# end
-
 # End AbstractMatrix interface
-
-
 
 
 function findgenokey(g::VCFGenotypeWrapper, key::String)
@@ -79,13 +30,9 @@ function findgenokey(g::VCFGenotypeWrapper, key::String)
     j
 end
 
-# function genotype_impl(g::VCFGenotypeWrapper, index::Int, keys::AbstractVector{Int})
-#     return [genotype_impl(g, index, k) for k in keys]
-# end
-
 function genotype_impl(g::VCFGenotypeWrapper, index::Int, key::Int)
     geno = g.genotype[index]
-    if key > lastindex(geno)  # dropped field
+    if key > lastindex(geno) # dropped field
         return "."
     else
         return String(g.data[geno[key]])
